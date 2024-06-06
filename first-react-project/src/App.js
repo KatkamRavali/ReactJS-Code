de8420b470348ea0c1c11d9f74730e62f9d2cc45
KatkamRavali <ravalikatkam681@gmail.com>
2024-06-06 18:11:50 +0530

    Completed Routing Part

--- first-react-project/src/App.js
+++ first-react-project/src/App.js
@@ -33,7 +33,7 @@ function App() {
             }
           />
           <Route
-            path="/updateUsigData"
+            path="/updateUsingData"
             element={
               <FunctionComponent2
                 name={name}
