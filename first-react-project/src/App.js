8383b74ef4a2714964cc46b17778aea87bf875af
KatkamRavali <ravalikatkam681@gmail.com>
2024-06-06 17:56:13 +0530

    Completed Routing Part

--- first-react-project/src/App.js
+++ first-react-project/src/App.js
@@ -1,12 +1,14 @@
+// ---------------------------- Routes -------------------------------
+
 import "./App.css";
 
-// Importing call component
 import ClassComponent from "./Components/ClassComponent";
 import FunctionComponent from "./Components/FunctionComponent";
 import FunctionComponent1 from "./Components/PropsFunctionComponent";
 import FunctionComponent2 from "./Components/UpdatingData";
 import FunctionComponent3 from "./Components/UpdateUsingButton";
 import FunctionComponent4 from "./Components/UpdateUsingInput";
+import { Routes, Route } from "react-router-dom";
 
 // ---- Updating a value ----
 import { useState } from "react";
@@ -17,45 +19,52 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p className="App-P"> Class Components </p>
-        <ClassComponent />
-        <br />
-
-        <p className="App-P"> Function Components </p>
-        <FunctionComponent />
-        <br />
-
-        <p className="App-P"> Using Props </p>
-        <FunctionComponent1
-          name="Ravali"
-          age={20}
-          graduation="St Francis College for women"
-        />
-
-        <p className="App-P">
-          Updating the name data internally and adding course data externally
-        </p>
-        <FunctionComponent2
-          name={name}
-          age={20}
-          graduation="St Francis College for women"
-        />
-
-        <p className="App-P"> Updating the data using button </p>
-        <FunctionComponent3
-          name={name}
-          age={20}
-          graduation="St Francis College for women"
-          setName={setName}
-        />
-
-        <p className="App-P"> Updating the data using input </p>
-        <FunctionComponent4
-          name={names}
-          age={20}
-          graduation="St Francis College for women"
-          setName={setNames}
-        />
+        <Routes>
+          <Route path="/" element={<ClassComponent />} />
+          <Route path="/function-component" element={<FunctionComponent />} />
+          <Route
+            path="/prop"
+            element={
+              <FunctionComponent1
+                name={name}
+                age={20}
+                graduation="St Francis College for women"
+              />
+            }
+          />
+          <Route
+            path="/updateUsigData"
+            element={
+              <FunctionComponent2
+                name={name}
+                age={20}
+                graduation="St Francis College for women"
+              />
+            }
+          />
+          <Route
+            path="/updateUsingButton"
+            element={
+              <FunctionComponent3
+                name={name}
+                age={20}
+                graduation="St Francis College for women"
+                setName={setName}
+              />
+            }
+          />
+          <Route
+            path="/updateUsingInput"
+            element={
+              <FunctionComponent4
+                name={names}
+                age={20}
+                graduation="St Francis College for women"
+                setName={setNames}
+              />
+            }
+          />
+        </Routes>
       </header>
     </div>
   );
